fb7ce8d883948e6069b7c86f828aeeeda8b32174
rebekav <vujcin.sf3.2019@uns.ac.rs>
2023-01-19 21:46:05 +0100

    initialize project

---
+++ src/main/java/com/ftn/poslovnainformatika/PoslovnaInformatikaApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.poslovnainformatika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoslovnaInformatikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoslovnaInformatikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ftn/poslovnainformatika/PoslovnaInformatikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.poslovnainformatika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoslovnaInformatikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
