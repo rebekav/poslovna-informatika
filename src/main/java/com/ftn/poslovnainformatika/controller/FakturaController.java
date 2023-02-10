9d45b2bd0515b06bb89ae39b26e7cff90726fd97
Igor Milakovic <igor.milakovic@itengine.rs>
2023-02-10 17:19:19 +0100

    implement pdf export for otpremnica & fakture

--- src/main/java/com/ftn/poslovnainformatika/controller/FakturaController.java
+++ src/main/java/com/ftn/poslovnainformatika/controller/FakturaController.java
@@ -4,8 +4,11 @@ import com.ftn.poslovnainformatika.dto.FakturaDTO;
 import com.ftn.poslovnainformatika.mapper.*;
 import com.ftn.poslovnainformatika.model.Faktura;
 import com.ftn.poslovnainformatika.model.StavkaFakture;
+import com.ftn.poslovnainformatika.pdfExporter.PdfIzlaznaFakturaExporter;
+import com.ftn.poslovnainformatika.pdfExporter.PdfUlaznaFakturaExporter;
 import com.ftn.poslovnainformatika.service.IFakturaService;
 import com.ftn.poslovnainformatika.service.IStavkeFakture;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -64,5 +68,32 @@ public class FakturaController {
         model.addAttribute("stavkeFakture", stavkaFaktureToStavkaFaktureDTO.konvertujEntityToDto(stavkeFakture));
         model.addAttribute("poslovniPartner", poslovniPartnerToPoslovniPartnerDTO.konvertujEntityToDto(faktura.getPoslovniPartner()));
         return "faktura_detalji";
+    }
+
+    @GetMapping("/faktura/pdf/{idFakture}")
+    public void exportToPDF(HttpServletResponse response, @PathVariable int idFakture)
+            throws DocumentException, IOException {
+        response.setContentType("application/pdf");
+
+        Faktura faktura = fakturaService.getOne(idFakture);
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=" + faktura.getBrojFakture() + "_"
+                + faktura.getPoslovnaGodina().getGodinaPoslovanja() + ".pdf";
+        response.setHeader(headerKey, headerValue);
+
+        List<StavkaFakture> stavkeFakture = stavkeFaktureService.findAll().stream()
+                .filter(sn -> sn.getFaktura().getId() == idFakture).collect(Collectors.toList());
+
+        FakturaDTO fakturaDTO = fakturaToFakturaDTO.konvertujEntityToDto(faktura);
+
+        if (fakturaDTO.isVrstaFakture()) {
+            PdfUlaznaFakturaExporter exporter = new PdfUlaznaFakturaExporter(stavkeFakture, faktura);
+            exporter.export(response);
+        } else {
+            PdfIzlaznaFakturaExporter exporter = new PdfIzlaznaFakturaExporter(stavkeFakture, faktura);
+            exporter.export(response);
+        }
+
     }
 }
---
+++ src/main/java/com/ftn/poslovnainformatika/pdfExporter/PdfIzlaznaFakturaExporter.java
@@ -0,0 +1,193 @@
+package com.ftn.poslovnainformatika.pdfExporter;
+
+import com.ftn.poslovnainformatika.model.Faktura;
+import com.ftn.poslovnainformatika.model.StavkaFakture;
+import com.lowagie.text.Document;
+
+import java.awt.*;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PdfIzlaznaFakturaExporter {
+
+    private List<StavkaFakture> stavkeFakture;
+    private Faktura faktura;
+
+    public PdfIzlaznaFakturaExporter(List<StavkaFakture> stavkeFakture, Faktura faktura) {
+        this.stavkeFakture = stavkeFakture;
+        this.faktura = faktura;
+    }
+
+    private void writeFakturaHeader(Document document) {
+
+        PdfPTable table = new PdfPTable(new float[] { 12, 12 });
+        table.setWidthPercentage(100);
+        table.getDefaultCell().setBorder(0);
+
+        table.addCell("Izdavalac racuna: \n" + faktura.getPoslovniPartner().getNazivPoslovnogPartnera());
+        table.addCell("Kupac: \n" + faktura.getPreduzece().getNazivPreduzeca());
+        table.addCell("Mesto i adresa: \n" + faktura.getPoslovniPartner().getAdresa() + " " + " "
+                + faktura.getPoslovniPartner().getMesto().getPostanskiBroj() + " "
+                + faktura.getPoslovniPartner().getMesto().getGrad());
+        table.addCell("Mesto i adresa: \n" + faktura.getPreduzece().getAdresa() + " " + " "
+                + faktura.getPreduzece().getMesto().getPostanskiBroj() + " "
+                + faktura.getPreduzece().getMesto().getGrad());
+        table.addCell("PIB: \n" + faktura.getPoslovniPartner().getPIB());
+        table.addCell("PIB: \n" + faktura.getPreduzece().getPIB());
+        table.addCell("Tekuci racun: \n" + faktura.getPoslovniPartner().getTekuciRacun());
+        table.addCell("Tekuci racun: \n" + faktura.getPreduzece().getTekuciRacun());
+
+        document.add(table);
+    }
+
+    private void writeTableHeader(PdfPTable table) {
+        PdfPCell cell = new PdfPCell();
+        cell.setBackgroundColor(Color.LIGHT_GRAY);
+        cell.setPadding(5);
+
+        Font font = FontFactory.getFont(FontFactory.HELVETICA);
+        font.setSize(12);
+        font.setColor(Color.black);
+
+        cell.setPhrase(new Phrase("R br.", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Sifra stavke", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Naziv robe", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Jednica mere", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Kolicina", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Stopa PDV", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Cena po komadu", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Rabat", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Osnovica", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("PDV", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Ukupan iznos", font));
+        table.addCell(cell);
+    }
+
+    private void writeTableData(PdfPTable table) {
+
+        int redniBroj = 1;
+
+        for (StavkaFakture stavkaFakture : stavkeFakture) {
+            table.addCell(String.valueOf(redniBroj));
+            table.addCell(String.valueOf(stavkaFakture.getId()));
+            table.addCell(stavkaFakture.getRoba().getNazivRobe());
+            table.addCell(stavkaFakture.getRoba().getJedinicaMere());
+            table.addCell(String.valueOf(stavkaFakture.getKolicina()));
+            table.addCell(String.valueOf(stavkaFakture.getProcenatPDV()));
+            table.addCell(String.valueOf(stavkaFakture.getCena()));
+            table.addCell(String.valueOf(stavkaFakture.getRabat()));
+            table.addCell(String.valueOf(stavkaFakture.getOsnovicaPDV()));
+            table.addCell(String.valueOf(stavkaFakture.getIznosPDV()));
+            table.addCell(String.valueOf(stavkaFakture.getUkupanIznos()));
+            redniBroj++;
+        }
+
+        PdfPCell cell = new PdfPCell();
+        cell.setBackgroundColor(Color.MAGENTA);
+
+        table.addCell("");
+        table.addCell("");
+        table.addCell("");
+        table.addCell("");
+        table.addCell("");
+        table.addCell("");
+        cell.setPhrase(new Phrase("\u03a3"));
+        table.addCell(cell);
+        cell.setPhrase(new Phrase(String.valueOf(faktura.getRabat())));
+        table.addCell(cell);
+        cell.setPhrase(new Phrase(String.valueOf(faktura.getOsnovica())));
+        table.addCell(cell);
+        cell.setPhrase(new Phrase(String.valueOf(faktura.getUkupanPdv())));
+        table.addCell(cell);
+        cell.setPhrase(new Phrase(String.valueOf(faktura.getIznosZaPlacanje())));
+        table.addCell(cell);
+    }
+
+    private void writeFakturaFooter(Document document) {
+
+        PdfPTable table = new PdfPTable(new float[] { 12, 12 });
+        table.setWidthPercentage(100);
+        table.getDefaultCell().setBorder(0);
+
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+
+        table.addCell("Broj fakture: \n" + faktura.getId() + "/" + faktura.getPoslovnaGodina().getGodinaPoslovanja() + "\n");
+        table.addCell("Rabat: \n" + faktura.getRabat() + "\n___________________________");
+        table.addCell("Datum fakture: \n" + dateFormatter.format(faktura.getDatumFakture()) + "\n");
+        table.addCell("Osnovica PDV: \n" + faktura.getOsnovica() + "\n___________________________");
+        table.addCell("Datum valute: \n" + dateFormatter.format(faktura.getDatumValute()) + "\n");
+        table.addCell("PDV: \n" + faktura.getUkupanPdv() + "\n___________________________");
+        table.addCell("");
+        table.addCell("Ukupno za placanje: \n" + faktura.getIznosZaPlacanje() + "\n___________________________");
+
+        document.add(table);
+    }
+
+    public void export(HttpServletResponse response) throws DocumentException, IOException {
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, response.getOutputStream());
+
+        document.open();
+        Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+        font.setSize(14);
+        font.setColor(Color.BLUE);
+
+        Paragraph p = new Paragraph(
+                "Faktura br. " + faktura.getBrojFakture() + "/" + faktura.getPoslovnaGodina().getGodinaPoslovanja(),
+                font);
+        p.setAlignment(Paragraph.ALIGN_CENTER);
+
+        document.add(p);
+
+        document.add(Chunk.NEWLINE);
+        writeFakturaHeader(document);
+
+        PdfPTable tableHead = new PdfPTable(2);
+        tableHead.setWidthPercentage(100f);
+
+        for (int i = 0; i < 2; i++) {
+            document.add(Chunk.NEWLINE);
+        }
+
+        PdfPTable table = new PdfPTable(11);
+        table.setWidthPercentage(100f);
+//        table.setWidths(new float[] {1.5f, 3.5f, 3.0f, 3.0f, 1.5f});
+        table.setSpacingBefore(5);
+
+        writeTableHeader(table);
+        writeTableData(table);
+
+        document.add(table);
+
+        document.add(Chunk.NEWLINE);
+
+        writeFakturaFooter(document);
+
+        document.close();
+
+    }
+
+}
