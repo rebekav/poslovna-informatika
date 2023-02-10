11202ce4286eea9220f544822010bacc75b633bc
milakmilak <65683666+milakmilak@users.noreply.github.com>
2023-02-10 17:20:44 +0100

    implement pdf export for otpremnica & fakture (#4)
    Co-authored-by: Igor Milakovic <milakovic.sf1.2019@uns.ac.rs>

--- src/main/java/com/ftn/poslovnainformatika/controller/OtpremnicaController.java
+++ src/main/java/com/ftn/poslovnainformatika/controller/OtpremnicaController.java
@@ -1,9 +1,25 @@
 package com.ftn.poslovnainformatika.controller;
 
 import com.ftn.poslovnainformatika.dto.OtpremnicaDTO;
-import com.ftn.poslovnainformatika.mapper.*;
-import com.ftn.poslovnainformatika.model.*;
-import com.ftn.poslovnainformatika.service.*;
+import com.ftn.poslovnainformatika.mapper.OtpremnicaDTOToOtpremnica;
+import com.ftn.poslovnainformatika.mapper.OtpremnicaToOtpremnicaDTO;
+import com.ftn.poslovnainformatika.mapper.PoslovniPartnerToPoslovniPartnerDTO;
+import com.ftn.poslovnainformatika.mapper.StavkaOtpremniceDTOToStavkaOtpremnice;
+import com.ftn.poslovnainformatika.mapper.StavkaOtpremniceToStavkaOtpremniceDTO;
+import com.ftn.poslovnainformatika.model.Otpremnica;
+import com.ftn.poslovnainformatika.model.PoslovnaGodina;
+import com.ftn.poslovnainformatika.model.PoslovniPartner;
+import com.ftn.poslovnainformatika.model.Preduzece;
+import com.ftn.poslovnainformatika.model.Roba;
+import com.ftn.poslovnainformatika.model.StavkaOtpremnice;
+import com.ftn.poslovnainformatika.pdfExporter.PdfOtpremnicaExporter;
+import com.ftn.poslovnainformatika.service.IOtpremnicaService;
+import com.ftn.poslovnainformatika.service.IPoslovnaGodinaService;
+import com.ftn.poslovnainformatika.service.IPoslovniPartnerService;
+import com.ftn.poslovnainformatika.service.IPreduzeceService;
+import com.ftn.poslovnainformatika.service.IRobaService;
+import com.ftn.poslovnainformatika.service.IStavkaOtpremniceService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +27,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -87,12 +104,12 @@ public class OtpremnicaController {
         return "redirect:/otpremnice";
     }
 
-    @GetMapping("otpremnica/detalji/{idotpremnice}")
-    public String detaljiNarudzbenice(Model model, @PathVariable long idotpremnice) {
-        Otpremnica otpremnica = otpremnicaService.getOne(idotpremnice);
+    @GetMapping("otpremnica/detalji/{otpremnicaId}")
+    public String detaljiNarudzbenice(Model model, @PathVariable long otpremnicaId) {
+        Otpremnica otpremnica = otpremnicaService.getOne(otpremnicaId);
         List<Roba> robe = robaService.findAll();
         List<StavkaOtpremnice> stavkaOtpremnice = stavkaOtpremniceService.findAll().stream()
-                .filter(so -> so.getOtpremnica().getId() == idotpremnice)
+                .filter(so -> so.getOtpremnica().getId() == otpremnicaId)
                 .collect(Collectors.toList());
         model.addAttribute("otpremnica", otpremnicaToOtpremnicaDTO.konvertujEntityToDto(otpremnica));
         model.addAttribute("robe", robe);
@@ -101,15 +118,33 @@ public class OtpremnicaController {
         return "otpremnica_detalji";
     }
 
-    @GetMapping("/otpremnica/kreirajFakturu/{idOtpremnice}")
-    public String kreirajFakturuOdOtpremnice(Model model, @PathVariable long idOtpremnice) {
+    @GetMapping("/otpremnica/kreirajFakturu/{otpremnicaId}")
+    public String kreirajFakturuOdOtpremnice(Model model, @PathVariable long otpremnicaId) {
         PoslovnaGodina poslovnaGodina = poslovnaGodinaService.findByZakljucenaGodinaIsFalseAndObrisanoIsFalse().get(0);
         int poslednjaPoslovnjaGodina = poslovnaGodina.getFakture().size();
 
-        Otpremnica otpremnica = otpremnicaService.getOne(idOtpremnice);
+        Otpremnica otpremnica = otpremnicaService.getOne(otpremnicaId);
         OtpremnicaDTO otpremnicaDTO = otpremnicaToOtpremnicaDTO.konvertujEntityToDto(otpremnica);
         otpremnicaService.kreirajFakturuOdOtpremnice(otpremnicaDTO, poslednjaPoslovnjaGodina);
         return "redirect:/otpremnice";
+    }
+
+    @GetMapping("otpremnica/pdf/{otpremnicaId}")
+    public void exportToPdf(HttpServletResponse response, @PathVariable long otpremnicaId) throws DocumentException, IOException {
+        response.setContentType("application/pdf");
+
+        Otpremnica otpremnica = otpremnicaService.getOne(otpremnicaId);
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename="+ otpremnica.getBrojOtpremnice() + "_" + otpremnica.getPoslovnaGodina().getGodinaPoslovanja() + ".pdf";
+        response.setHeader(headerKey, headerValue);
+
+        List<StavkaOtpremnice> stavkeOtpremnice = stavkaOtpremniceService.findAll().stream()
+                .filter(so -> so.getOtpremnica().getId() == otpremnicaId)
+                .collect(Collectors.toList());
+
+        PdfOtpremnicaExporter exporter = new PdfOtpremnicaExporter(stavkeOtpremnice, otpremnica);
+        exporter.export(response);
     }
 
 }
---
+++ src/main/java/com/ftn/poslovnainformatika/pdfExporter/PdfOtpremnicaExporter.java
@@ -0,0 +1,157 @@
+package com.ftn.poslovnainformatika.pdfExporter;
+
+import com.ftn.poslovnainformatika.model.Otpremnica;
+import com.ftn.poslovnainformatika.model.StavkaOtpremnice;
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+import com.lowagie.text.pdf.draw.LineSeparator;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.awt.*;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class PdfOtpremnicaExporter {
+
+    private List<StavkaOtpremnice> stavkeOtpremnice;
+    private Otpremnica otpremnica;
+
+    public PdfOtpremnicaExporter(List<StavkaOtpremnice> stavkeOtpremnice, Otpremnica otpremnica) {
+        this.stavkeOtpremnice = stavkeOtpremnice;
+        this.otpremnica = otpremnica;
+    }
+
+    private void writeOtpremnicaHeader(Document document) {
+
+        PdfPTable table = new PdfPTable(new float[] { 12, 12 });
+        table.setWidthPercentage(100);
+        table.getDefaultCell().setBorder(0);
+
+        table.addCell(otpremnica.getPreduzece().getNazivPreduzeca());
+        table.addCell("Telefon: " + otpremnica.getPreduzece().getTelefon());
+        table.addCell(otpremnica.getPreduzece().getAdresa() + " " + " "
+                + otpremnica.getPreduzece().getMesto().getPostanskiBroj() + " "
+                + otpremnica.getPreduzece().getMesto().getGrad());
+        table.addCell("Tekuci racun: " + otpremnica.getPreduzece().getTekuciRacun());
+        table.addCell("PIB: " + otpremnica.getPreduzece().getPIB());
+        table.addCell("email: " + otpremnica.getPreduzece().getEmailAdresa());
+
+        document.add(table);
+    }
+
+    private void writeTableHeader(PdfPTable table) {
+        PdfPCell cell = new PdfPCell();
+        cell.setBackgroundColor(Color.LIGHT_GRAY);
+        cell.setPadding(5);
+
+        Font font = FontFactory.getFont(FontFactory.HELVETICA);
+        font.setSize(12);
+        font.setColor(Color.black);
+
+        cell.setPhrase(new Phrase("R br.", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Naziv robe", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Jednica mere", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Cena", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Kolicina", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Napomena", font));
+        table.addCell(cell);
+
+    }
+
+    private void writeTableData(PdfPTable table) {
+
+        int redniBroj = 1;
+
+        for (StavkaOtpremnice stavkaOtpremnice : stavkeOtpremnice) {
+            table.addCell(String.valueOf(redniBroj));
+            table.addCell(stavkaOtpremnice.getRoba().getNazivRobe());
+            table.addCell(stavkaOtpremnice.getRoba().getJedinicaMere());
+            table.addCell(String.valueOf(stavkaOtpremnice.getCena()));
+            table.addCell(String.valueOf(stavkaOtpremnice.getKolicina()));
+            table.addCell("");
+            redniBroj++;
+        }
+    }
+
+    private void writeOtpremnicaSecondHeader(Document document) {
+
+        PdfPTable table = new PdfPTable(new float[] { 12, 12 });
+        table.setWidthPercentage(100);
+        table.getDefaultCell().setBorder(0);
+
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+
+        String currentDateTime = dateFormatter.format(new Date());
+
+        table.addCell("Broj otpremnice: " + otpremnica.getBrojOtpremnice() + "/"
+                + otpremnica.getPoslovnaGodina().getGodinaPoslovanja() + "\n");
+        table.addCell("Datum izdavanja: " + currentDateTime);
+        table.addCell("Kupac: " + otpremnica.getPoslovniPartner().getNazivPoslovnogPartnera());
+        table.addCell(" ");
+        table.addCell("Adresa isporuke: \n" + otpremnica.getPoslovniPartner().getAdresa() + " "
+                + otpremnica.getPoslovniPartner().getMesto().getPostanskiBroj() + " "
+                + otpremnica.getPoslovniPartner().getMesto().getGrad());
+        table.addCell(" ");
+
+        document.add(table);
+    }
+
+    public void export(HttpServletResponse response) throws DocumentException, IOException {
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, response.getOutputStream());
+
+        document.open();
+        Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+        font.setSize(14);
+        font.setColor(Color.BLUE);
+
+        Paragraph p = new Paragraph("Otpremnica br. " + otpremnica.getBrojOtpremnice() + "/"
+                + otpremnica.getPoslovnaGodina().getGodinaPoslovanja(), font);
+        p.setAlignment(Paragraph.ALIGN_CENTER);
+
+        document.add(p);
+
+        document.add(Chunk.NEWLINE);
+        writeOtpremnicaHeader(document);
+
+        LineSeparator ls = new LineSeparator();
+        document.add(new Chunk(ls));
+        document.add(Chunk.NEWLINE);
+
+        writeOtpremnicaSecondHeader(document);
+
+        document.add(Chunk.NEWLINE);
+
+        PdfPTable table = new PdfPTable(6);
+        table.setWidthPercentage(100f);
+
+        table.setSpacingBefore(5);
+
+        writeTableHeader(table);
+        writeTableData(table);
+
+        document.add(table);
+
+        document.add(Chunk.NEWLINE);
+
+        document.close();
+
+    }
+
+}
